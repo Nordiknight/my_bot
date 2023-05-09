9ff847193de28fd3d656a88c60f5831a19fb20ed
Nordiknight <jingcheng729@gmail.com>
2023-05-09 12:33:28 -0400

    rotate with odom

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,11 +22,11 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
-
+    
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -40,7 +40,6 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
@@ -52,23 +51,6 @@ def generate_launch_description():
         executable="spawner.py",
         arguments=["joint_broad"],
     )
-
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
 
 
